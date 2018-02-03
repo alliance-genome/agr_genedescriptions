94e1d5768b49d0476bf5f2b63792d1d5ad0eea9d
Valerio Arnaboldi <valearna@caltech.edu>
2018-02-02 17:54:30 -0800

    bug fixing
    - fixed location of ini file for testing

--- tests/test_data_fetcher.py
+++ tests/test_data_fetcher.py
@@ -8,7 +8,7 @@ class TestWBRawDataFetcher(unittest.TestCase):
     def setUp(self):
         this_dir = os.path.split(__file__)[0]
         config = configparser.ConfigParser()
-        config.read(os.path.join(os.path.pardir(this_dir), "genedesc.ini"))
+        config.read(os.path.join(this_dir, os.path.pardir, "genedesc.ini"))
         self.raw_files_source = config.get("data_fetcher", "raw_files_source")
         self.cache_location = config.get("data_fetcher", "cache_location")
         self.species = config.get("generic", "species").split(",")[3]
