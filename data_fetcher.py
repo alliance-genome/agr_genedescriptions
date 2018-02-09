f63b87d57e5245ae6877c917029dca7b7154074b
Valerio Arnaboldi <valearna@caltech.edu>
2018-02-08 18:11:17 -0800

    added loaders for more ontologies

--- data_fetcher.py
+++ data_fetcher.py
@@ -2,17 +2,15 @@ import gzip
 import urllib.request
 from collections import namedtuple, defaultdict
 from enum import Enum
-
 import os
 from urllib.parse import urlparse
-
-from namedlist import namedlist
 from typing import List
+import logging
+import re
 
-Gene = namedtuple('Gene', ['id', 'name'])
+Gene = namedtuple('Gene', ['id', 'name', 'dead', 'pseudo'])
 GOAnnotation = namedtuple('GOAnnotation', ['go_id', 'qualifier', 'paper_reference', 'evidence_code', 'aspect',
                                            'annotation_ext', 'go_name', 'is_obsolete'])
-GOOntologyEntry = namedlist('GOOntologyEntry', 'name is_obsolete')
 
 
 class GO_ASPECT(Enum):
@@ -23,12 +21,14 @@ class GO_ASPECT(Enum):
 
 class WBRawDataFetcher:
     """data fetcher for WormBase raw files for a single species"""
-    def __init__(self, raw_files_source: str, cache_location: str, release_version: str, species: str, project_id: str,
-                 use_cache: bool, ):
+    def __init__(self, raw_files_source: str, chebi_files_source: str, cache_location: str, release_version: str,
+                 species: str, project_id: str, use_cache: bool, ):
         """create a new data fetcher
 
         :param raw_files_source: base url where to fetch the raw files
         :type raw_files_source: str
+        :param chebi_files_source: base url where to fetch the chebi files
+        :type chebi_files_source: str
         :param cache_location: path to cache directory
         :type cache_location: str
         :param release_version: WormBase release version for the input files
@@ -42,12 +42,17 @@ class WBRawDataFetcher:
         :type use_cache: bool
         """
         self.raw_files_source = raw_files_source
+        self.chebi_files_source = chebi_files_source
         self.cache_location = cache_location
         self.release_version = release_version
         self.species = species
         self.project_id = project_id
         self.go_data = defaultdict(set)
         self.go_ontology = {}
+        self.ls_ontology = {}
+        self.an_ontology = {}
+        self.gene_data = {}
+        self.chebi_ontology = {}
         self.use_cache = use_cache
 
     def _fill_cache_if_empty_and_activated(self, cache_url, file_source_url):
@@ -66,6 +71,14 @@ class WBRawDataFetcher:
         :return: data for one gene per each call, including gene_id and gene_name
         :rtype: Gene
         """
+        if len(self.gene_data) == 0:
+            self._load_gene_data()
+        for gene_id, gene_obj in self.gene_data.items():
+            if (include_dead_genes or not gene_obj.dead) and (include_pseudo_genes or not gene_obj.pseudo):
+                yield gene_obj
+
+    def _load_gene_data(self) -> None:
+        """load all gene data"""
         cache_url = os.path.join(self.cache_location, self.release_version, "species", self.species, self.project_id,
                                  "annotation", self.species + '.' + self.project_id + '.' + self.release_version +
                                  ".geneIDs.txt.gz")
@@ -74,20 +87,37 @@ class WBRawDataFetcher:
                          self.release_version + '.geneIDs.txt.gz'
         self._fill_cache_if_empty_and_activated(cache_url=cache_url, file_source_url=source_address)
         address = cache_url if self.use_cache else source_address
-        # TODO if include_pseudo_genes == False exclude it - get pseudo gene data from another file
         with urllib.request.urlopen(address) as url:
             gzip_file = gzip.GzipFile(fileobj=url)
             for line in gzip_file:
                 fields = line.decode("utf-8").strip().split(',')
-                if include_dead_genes or fields[4] != "Dead":
-                    name = fields[2] if fields[2] != '' else fields[3]
-                    yield Gene(fields[1], name)
+                name = fields[2] if fields[2] != '' else fields[3]
+                self.gene_data[fields[1]] = Gene(fields[1], name, fields[4] == "Dead", False)
 
     def load_go_data(self) -> None:
         """read go data and gene ontology. After calling this function, go annotations containing mapped go names can
         be retrieved by using the :meth:`data_fetcher.WBRawDataFetcher.get_go_annotations` function
         """
-        self._load_go_ontology()
+        self._load_ontology_data(url=self.raw_files_source + '/' + self.release_version + '/ONTOLOGY/gene_ontology.' + \
+                                 self.release_version + '.obo', cache_path=os.path.join(self.cache_location,
+                                                                                        self.release_version,
+                                                                                        "ONTOLOGY", "gene_ontology." +
+                                                                                        self.release_version + ".obo"),
+                                 field_names=["name", "is_obsolete"], ontology_dict=self.go_ontology, gzip_file=False)
+        self._load_ontology_data(url=self.raw_files_source + '/' + self.release_version +
+                                     '/ONTOLOGY/development_ontology.' + self.release_version + '.obo',
+                                 cache_path=os.path.join(self.cache_location, self.release_version, "ONTOLOGY",
+                                                         "development_ontology." + self.release_version + ".obo"),
+                                 field_names=["name"], ontology_dict=self.ls_ontology, gzip_file=False)
+        self._load_ontology_data(url=self.raw_files_source + '/' + self.release_version +
+                                     '/ONTOLOGY/anatomy_ontology.' + self.release_version + '.obo',
+                                 cache_path=os.path.join(self.cache_location, self.release_version, "ONTOLOGY",
+                                                         "anatomy_ontology." + self.release_version + ".obo"),
+                                 field_names=["name"], ontology_dict=self.an_ontology, gzip_file=False)
+        self._load_ontology_data(url=self.chebi_files_source + '/' + 'chebi_lite.obo.gz',
+                                 cache_path=os.path.join(self.cache_location, "CHEBI", "chebi_lite.obo.gz"),
+                                 field_names=["name"], ontology_dict=self.chebi_ontology, gzip_file=True)
+        self._load_gene_data()
         cache_url = os.path.join(self.cache_location, self.release_version, "species", self.species, self.project_id,
                                  "annotation", self.species + '.' + self.project_id + '.' + self.release_version +
                                  ".go_annotations.gaf.gz")
@@ -110,38 +140,74 @@ class WBRawDataFetcher:
                     elif fields[8] == 'P':
                         go_aspect = GO_ASPECT.BIOLOGICAL_PROCESS
                     is_obsolete = False
-                    if self.go_ontology[fields[4]].is_obsolete == 'true':
+                    if "is_obsolete" in self.go_ontology[fields[4]]["is_obsolete"] and \
+                            self.go_ontology[fields[4]]["is_obsolete"] == 'true':
                         is_obsolete = True
+                    extension = ""
+                    if fields[15] != "":
+                        extension = fields[15]
+                        matches = re.findall('(\([^\)]+\))', fields[15])
+                        for match in matches:
+                            ext_id = match[1:-1]
+                            ext_translation = self._ontology_translation(ext_id)
+                            extension = extension.replace(match, " " + ext_translation)
+                        logging.debug("Found GO annotation with field 16: " + fields[1] + "\t" + fields[2] + "\t" +
+                                      fields[3] + "\t" + self._map_id_ontology_name(fields[4], self.go_ontology) +
+                                      "\t" + fields[6] + "\t" + fields[7] + "\t" + fields[8] + "\t" + fields[14] +
+                                      "\t" + extension)
                     self.go_data[fields[1]].add(GOAnnotation(fields[4], fields[3], fields[5], fields[6], go_aspect,
-                                                                fields[15], self.go_ontology[fields[4]].name,
-                                                                is_obsolete))
+                                                             extension, self.go_ontology[fields[4]]["name"],
+                                                             is_obsolete))
 
-    def _load_go_ontology(self):
-        """read go ontology data"""
-        cache_url = os.path.join(self.cache_location, self.release_version, "ONTOLOGY", "gene_ontology." +
-                                       self.release_version + ".obo")
-        source_address = self.raw_files_source + '/' + self.release_version + '/ONTOLOGY/gene_ontology.' + \
-                         self.release_version + '.obo'
-        self._fill_cache_if_empty_and_activated(cache_url=cache_url, file_source_url=source_address)
-        address = cache_url if self.use_cache else source_address
+    @staticmethod
+    def _map_id_ontology_name(ont_id: str, ontology_dict: dict):
+        if ont_id in ontology_dict:
+            return ontology_dict[ont_id]["name"]
+        else:
+            return ont_id
+
+    def _ontology_translation(self, ont_id):
+        if ont_id.startswith("WBls:"):
+            return self._map_id_ontology_name(ont_id, self.ls_ontology)
+        elif ont_id.startswith("WBbt:"):
+            return self._map_id_ontology_name(ont_id, self.an_ontology)
+        elif ont_id.startswith("GO:"):
+            return self._map_id_ontology_name(ont_id, self.go_ontology)
+        elif ont_id.startswith("ChEBI:"):
+            return self._map_id_ontology_name(ont_id, self.chebi_ontology)
+        elif ont_id.startswith("WB:WBGene"):
+            if ont_id in self.gene_data:
+                return self.gene_data[ont_id].name
+            else:
+                return ont_id
+        else:
+            return ont_id
+
+    def _load_ontology_data(self, url: str, cache_path: str, field_names: List[str], ontology_dict: dict,
+                            gzip_file: bool = False):
+        """read ontology data"""
+        self._fill_cache_if_empty_and_activated(cache_url=cache_path, file_source_url=url)
+        address = cache_path if self.use_cache else url
         with urllib.request.urlopen(address) as url:
-            go_id = None
-            go_entry = GOOntologyEntry(None, None)
+            if gzip_file:
+                url = gzip.GzipFile(fileobj=url)
+            ont_id = None
+            ont_entry = defaultdict(str)
             for line in url:
                 line = line.decode("utf-8")
                 if line.strip() == '[Term]':
-                    if go_entry.name is not None and go_id is not None:
-                        self.go_ontology[go_id] = go_entry
-                        go_id = None
-                        go_entry = GOOntologyEntry(None, None)
+                    if ont_id is not None:
+                        ontology_dict[ont_id] = ont_entry
+                        ont_id = None
+                        ont_entry = {"is_obsolete": "false"}
                 else:
                     fields = line.strip().split(": ")
                     if fields[0] == "id":
-                        go_id = fields[1]
-                    elif fields[0] == "name":
-                        go_entry.name = fields[1]
-                    elif fields[0] == "is_obsolete":
-                        go_entry.is_obsolete = fields[1]
+                        ont_id = fields[1]
+                    else:
+                        for fn in field_names:
+                            if fields[0] == fn:
+                                ont_entry[fn] = fields[1]
 
     def get_go_annotations(self, geneid: str, include_obsolete: bool = False,
                            priority_list: tuple = ("EXP", "IDA", "IPI", "IMP", "IGI", "IEP", "IC", "ISS", "ISO", "ISA",
--- generate_descriptions.py
+++ generate_descriptions.py
@@ -4,6 +4,7 @@ import argparse
 import configparser
 from data_fetcher import WBRawDataFetcher
 from descriptions_rules import *
+import logging
 
 
 def main():
@@ -13,6 +14,10 @@ def main():
     parser.add_argument("-C", "--use-cache", dest="use_cache", action="store_true", default=False,
                         help="Use cached source files from cache_location specified in config file. Download them from "
                              "raw_file_source (configured in config file) if not yet cached")
+    parser.add_argument("-l", "--log-file", metavar="log_file", dest="log_file", type=str,
+                        default="genedescriptions.log", help="path to the log file to generate")
+    parser.add_argument("-L", "--log-level", dest="log_level", choices=['DEBUG', 'INFO', 'WARNING', 'ERROR',
+                                                                        'CRITICAL'], help="set the logging level")
     parser.add_argument("-v", "--output-version", metavar="version_number", dest="version_number", type=str,
                         help="release version number")
     parser.add_argument("-w", "--wormbase-version", metavar="wormbase_number", dest="wormbase_number", type=str,
@@ -23,13 +28,18 @@ def main():
     config = configparser.ConfigParser()
     config.read(args.config_file)
 
+    logging.basicConfig(filename=args.log_file, level=args.log_level)
+
     raw_files_source = config.get("data_fetcher", "raw_files_source")
+    chebi_files_source = config.get("data_fetcher", "chebi_files_source")
     cache_location = config.get("data_fetcher", "cache_location")
     species = config.get("generic", "species").split(",")
     project_ids = config.get("generic", "project_ids").split(",")
 
-    df = WBRawDataFetcher(raw_files_source=raw_files_source, release_version=args.wormbase_number, species=species[3],
+    df = WBRawDataFetcher(raw_files_source=raw_files_source, chebi_files_source=chebi_files_source,
+                          release_version=args.wormbase_number, species=species[3],
                           project_id=project_ids[3], cache_location=cache_location, use_cache=args.use_cache)
+
     df.load_go_data()
     for gene in df.get_gene_data():
         print(gene.id, gene.name)
