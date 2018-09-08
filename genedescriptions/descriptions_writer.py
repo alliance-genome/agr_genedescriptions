e3206f311e1fa22dc5350de2908410362eb0a631
Valerio Arnaboldi <valearna@caltech.edu>
2018-09-08 14:19:30 -0700

    added Gene id and removed description without evidences in ace writer

--- genedescriptions/descriptions_writer.py
+++ genedescriptions/descriptions_writer.py
@@ -142,7 +142,7 @@ class DescriptionsWriter(object):
             outfile.write("\n")
             for genedesc in self.data:
                 if genedesc.description:
-                    outfile.write("Automated_description\t\"" + genedesc.description + "\"\n")
+                    outfile.write("Gene : \"" + genedesc.gene_id[3:] + "\"\n")
                     # for evidence in genedesc.evidences:
                     #    accession_arr = evidence.split(":")
                     #    outfile.write("Automated_description\t\"" + genedesc.description +
