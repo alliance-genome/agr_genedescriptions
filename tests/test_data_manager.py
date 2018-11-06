24bf205acff0a0531ac11784bc6c9f631adc2533
Valerio Arnaboldi <valearna@caltech.edu>
2018-11-06 12:10:49 -0800

    fixed travis tests
    removed coverage for pyYaml conflicts

--- tests/test_data_manager.py
+++ tests/test_data_manager.py
@@ -11,13 +11,13 @@ logger = logging.getLogger("Gene Descriptions tests")
 class TestRawDataFetcher(unittest.TestCase):
 
     def setUp(self):
-        logging.basicConfig(filename=None, level="DEBUG", format='%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+        logging.basicConfig(filename=None, level="ERROR", format='%(asctime)s - %(name)s - %(levelname)s: %(message)s')
         logger.info("Starting DataManager tests")
         self.this_dir = os.path.split(__file__)[0]
         self.conf_parser = GenedescConfigParser(os.path.join(self.this_dir, os.path.pardir, "tests", "config_test.yml"))
         self.df = DataManager(do_relations=None, go_relations=["subClassOf", "BFO:0000050"])
 
-    def test_load_ontology_from_file(self):
+    def test_load_from_file(self):
         logger.info("Testing loading go ontology from file")
         self.df.load_ontology_from_file(ontology_type=DataType.GO, ontology_url="file://" + os.path.join(
             self.this_dir, os.path.pardir, "tests", "data", "go.obo"),
@@ -26,3 +26,10 @@ class TestRawDataFetcher(unittest.TestCase):
         self.assertTrue(self.df.go_ontology is not None)
         self.assertTrue(any(parent == "GO:0007052" for parent in
                             self.df.go_ontology.parents("GO:0000022")))
+        logger.info("Testing loading go associations from file")
+        self.df.load_associations_from_file(associations_type=DataType.GO, associations_url="file://" + os.path.join(
+            self.this_dir, os.path.pardir, "tests", "data", "go_annotations.gaf"),
+                                            associations_cache_path=os.path.join(self.this_dir, os.path.pardir, "tests",
+                                                                                 "cache", "go_annotations.gaf"),
+                                            config=self.conf_parser)
+        self.assertTrue(self.df.go_associations is not None)
