c1a6b36afcc464fd518c6409c577249c84144838
Valerio Arnaboldi <valearna@caltech.edu>
2019-02-15 16:22:24 -0800

    new test data

--- tests/data/get_gaf_onto_subset.sh
+++ tests/data/get_gaf_onto_subset.sh
@@ -6,7 +6,7 @@ then
     exit 1
 fi
 
-genes='HTT\|Htt\|htt\|CFTR\|Cftr\|cftr\|YTA12\|tefu\|ATM\|ATM1\|Atm\|MCM1\|Srf\|SRF\|srfb\|REB1\|daf-16\|aap-1\|abl-1\|pat-4\|Pten\|PTEN\|Ahr\|AHR\|CDC28\|CDC48\|CDC5\|SGS1\|LAS17\|RAD27\|MLH1\|RAD53\|CCC2\|COQ6\|TRS20\|PXA1\|DPB3\|lsd-1\|let-60\|MPZ\|ntn1b\|let-60\|alg-1'
+genes='HTT\|Htt\|htt\|CFTR\|Cftr\|cftr\|YTA12\|tefu\|ATM\|ATM1\|Atm\|MCM1\|Srf\|SRF\|srfb\|REB1\|daf-16\|aap-1\|abl-1\|pat-4\|Pten\|PTEN\|Ahr\|AHR\|CDC28\|CDC48\|CDC5\|SGS1\|LAS17\|RAD27\|MLH1\|RAD53\|CCC2\|COQ6\|TRS20\|PXA1\|DPB3\|lsd-1\|let-60\|MPZ\|ntn1b\|let-60\|alg-1\|UBP6\|GPM2'
 
 for assoc_file in $1/*
 do
@@ -45,6 +45,7 @@ echo "http://purl.obolibrary.org/obo/GO_0043055" >> ontofox_script.txt
 echo "http://purl.obolibrary.org/obo/GO_0061065" >> ontofox_script.txt
 echo "http://purl.obolibrary.org/obo/GO_0043054" >> ontofox_script.txt
 echo "http://purl.obolibrary.org/obo/GO_0043053" >> ontofox_script.txt
+echo "http://purl.obolibrary.org/obo/GO_0006096" >> ontofox_script.txt
 
 echo >> ontofox_script.txt
 
