9c7262966f34eaca11b6040a4941d0799677f233
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2019-10-21 16:08:26 -0700

    fix: set common root to first node id if property not set in nodes

--- genedescriptions/ontology_tools.py
+++ genedescriptions/ontology_tools.py
@@ -33,7 +33,9 @@ def nodes_have_same_root(node_ids: List[str], ontology: Ontology) -> Union[bool,
                     if common_root and common_root != basic_prop_val["val"]:
                         return False
                     common_root = basic_prop_val["val"]
-    return True
+    if common_root is None:
+        common_root = node_ids[0]
+    return common_root
 
 
 def get_all_common_ancestors(node_ids: List[str], ontology: Ontology, min_distance_from_root: int = 0,
