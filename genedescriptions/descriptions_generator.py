08f442810037907bcdb5f198082aed0acca0ec89
Valerio Arnaboldi <valearna@caltech.edu>
2020-03-31 16:33:47 -0700

    fix: restored case sensitive qualifiers

--- genedescriptions/descriptions_generator.py
+++ genedescriptions/descriptions_generator.py
@@ -87,7 +87,7 @@ class OntologySentenceGenerator(object):
                 if annotation["evidence"]["type"] in evidence_codes_groups_map:
                     aspect = annotation["aspect"]
                     ev_group = evidence_codes_groups_map[annotation["evidence"]["type"]]
-                    qualifier = "_".join(sorted([q.lower().replace(" ", "_") for q in annotation["qualifiers"]])) if \
+                    qualifier = "_".join(sorted([q.replace(" ", "_") for q in annotation["qualifiers"]])) if \
                         "qualifiers" in annotation else ""
                     if prepostfix_special_cases_sent_map and (aspect, ev_group, qualifier) in \
                         prepostfix_special_cases_sent_map:
