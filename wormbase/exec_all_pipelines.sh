ae5a2cc1a4dd264671b2ae43dbd2998ff32f519c
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2023-11-28 09:44:47 -0800

    Update Dockerfile and exec_all_pipelines.sh script
    Updated `Dockerfile` to include the installations of necessary packages, the `owltools` download and setting its permissions. Also, `exec_all_pipelines.sh` script has been modified to streamline the folder creation process and to correct the paths to the project files. The changes implemented aim to ensure smooth deployment and operation of the Wormbase project container.

--- wormbase/exec_all_pipelines.sh
+++ wormbase/exec_all_pipelines.sh
@@ -1,12 +1,10 @@
-#!/usr/bin/env bash
-
 export PYTHONPATH=$PYTHONPATH:/usr/src/gene_descriptions
 
-python3 /usr/src/gene_descrptions/wormbase_pipeline.py -c /usr/src/gene_descrptions/wormbase/config_wb.yml -L DEBUG -o json txt tsv ace -t $1
-cat /usr/caltech_curation_files/pub/gene_descrpitons/${WB_RELEASE}/pre-release/*.ace > automatedesc.ace
-rm /usr/caltech_curation_files/pub/gene_descrpitons/${WB_RELEASE}/pre-release/*.ace
 today_folder=$(date '+%Y%m%d')
-mkdir /usr/caltech_curation_files/pub/gene_descrpitons/${WB_RELEASE}/pre-release/${today_folder}
-mv automatedesc.ace /usr/caltech_curation_files/pub/gene_descrpitons/${WB_RELEASE}/pre-release/${today_folder}
-python3 /usr/src/gene_descrptions/manualdesc_postgres2ace.py > /usr/caltech_curation_files/pub/gene_descrpitons/${WB_RELEASE}/pre-release/${today_folder}/${today_folder}_manualdesc.ace
-python3 /usr/src/gene_descrptions/create_overall_stats_file.py -i /usr/caltech_curation_files/pub/gene_descrpitons/${WB_RELEASE}/pre-release/${today_folder} -m /usr/caltech_curation_files/pub/gene_descrpitons/${WB_RELEASE}/pre-release/${today_folder}/${today_folder}_manualdesc.ace > /usr/caltech_curation_files/pub/gene_descrpitons/${WB_RELEASE}/pre-release/${today_folder}/${today_folder}__number_of_concise_descriptions.txt
+mkdir -p /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/${today_folder}
+python3 /usr/src/gene_descriptions/wormbase/wormbase_pipeline.py -c /usr/src/gene_descriptions/wormbase/config_wb.yml -L DEBUG -o json txt tsv ace -t ${TEXTPRESSO_TOKEN}
+cat /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/*.ace > automatedesc.ace
+rm /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/*.ace
+mv automatedesc.ace /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/${today_folder}
+python3 /usr/src/gene_descriptions/manualdesc_postgres2ace.py > /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/${today_folder}/${today_folder}_manualdesc.ace
+python3 /usr/src/gene_descriptions/create_overall_stats_file.py -i /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/${today_folder} -m /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/${today_folder}/${today_folder}_manualdesc.ace > /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/${today_folder}/${today_folder}__number_of_concise_descriptions.txt
