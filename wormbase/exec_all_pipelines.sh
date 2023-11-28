a9c2dfe2805e65b483b74d58d6f81f9d6858bea9
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2023-11-28 11:32:38 -0800

    Move automatedesc.ace to timestamped subdirectory
    Updated the exec_all_pipelines.sh script to enhance data organization and retrieval. The automatedesc.ace file is now created directly inside today's timestamped folder, thus specifying the date for each created .ace file. Also moved all *.json, *.txt, *.tsv files into the timestamped subdirectory to keep all variants of data together.

--- wormbase/exec_all_pipelines.sh
+++ wormbase/exec_all_pipelines.sh
@@ -3,8 +3,8 @@ export PYTHONPATH=$PYTHONPATH:/usr/src/gene_descriptions
 today_folder=$(date '+%Y%m%d')
 mkdir -p /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/${today_folder}
 python3 /usr/src/gene_descriptions/wormbase/wormbase_pipeline.py -c /usr/src/gene_descriptions/wormbase/config_wb.yml -L DEBUG -o json txt tsv ace -t ${TEXTPRESSO_TOKEN}
-cat /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/*.ace > automatedesc.ace
+cat /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/*.ace > /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/${today_folder}/${today_folder}_automatedesc.ace
 rm /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/*.ace
-mv automatedesc.ace /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/${today_folder}
+mv /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/*.{json,txt,tsv} /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/${today_folder}
 python3 /usr/src/gene_descriptions/manualdesc_postgres2ace.py > /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/${today_folder}/${today_folder}_manualdesc.ace
 python3 /usr/src/gene_descriptions/create_overall_stats_file.py -i /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/${today_folder} -m /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/${today_folder}/${today_folder}_manualdesc.ace > /usr/caltech_curation_files/pub/gene_descriptions/${WB_RELEASE}/pre-release/${today_folder}/${today_folder}__number_of_concise_descriptions.txt
