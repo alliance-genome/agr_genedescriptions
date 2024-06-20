50c1bf02619c080dec2c32ae989989d056cf38f6
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2024-06-20 14:37:44 -0700

    Removed recursion from ontology functions

--- genedescriptions/ontology_tools.py
+++ genedescriptions/ontology_tools.py
@@ -1,8 +1,9 @@
 """set of functions to manipulate ontology graphs"""
 import logging
 import math
+import time
 from collections import defaultdict
-from typing import List, Union, Dict, Any
+from typing import List, Union
 
 from ontobio.assocmodel import AssociationSet
 from ontobio.ontol import Ontology
@@ -71,20 +72,24 @@ def get_all_common_ancestors(node_ids: List[str], ontology: Ontology, min_distan
             ancestors.items() if len(covered_nodes) > 1 or ancestor == covered_nodes[0]]
 
 
-def set_all_depths(ontology: Ontology, relations: List[str] = None, comparison_func=max):
-    for root_id in ontology.get_roots():
+def set_all_depths(ontology: Ontology, root_node_ids: List[str], relations: List[str] = None, comparison_func=max):
+    logger.info("Setting depth for all nodes")
+    start_time = time.time()
+    for root_id in root_node_ids:
         if "type" not in ontology.node(root_id) or ontology.node_type(root_id) == "CLASS":
             set_all_depths_in_subgraph(ontology=ontology, root_id=root_id, relations=relations,
                                        comparison_func=comparison_func)
     for node_id, node_content in ontology.nodes().items():
         if "depth" not in node_content:
             node_content["depth"] = 0
+    logger.info(f"setting all depths took {time.time() - start_time} seconds")
 
 
 def set_all_depths_in_subgraph(ontology: Ontology, root_id: str, relations: List[str] = None, comparison_func=max,
                                current_depth: int = 0):
-    """calculate and set max_depth and min_depth (maximum and minimum distances from root terms in the ontology)
-    recursively for all terms in a branch of the ontology
+    """
+    Calculate and set max_depth and min_depth (maximum and minimum distances from root terms in the ontology)
+    for all nodes in a branch of the ontology
 
     Args:
         ontology (Ontology): the ontology
@@ -94,41 +99,40 @@ def set_all_depths_in_subgraph(ontology: Ontology, root_id: str, relations: List
             the root. max calculates the length of the longest path, min the one of the shortest
         current_depth (int): the current depth in the ontology
     """
-    if "depth" not in ontology.node(root_id):
-        ontology.node(root_id)["depth"] = current_depth
-    else:
-        ontology.node(root_id)["depth"] = comparison_func(ontology.node(root_id)["depth"], current_depth)
-    children = set(ontology.children(node=root_id, relations=relations))
-    children.discard(root_id)
-    children = list(children)
-    for child_id in children:
-        set_all_depths_in_subgraph(ontology=ontology, root_id=child_id, relations=relations,
-                                   comparison_func=comparison_func, current_depth=current_depth + 1)
+    stack = [(root_id, current_depth)]
+    while stack:
+        node_id, current_depth = stack.pop()
+        if "depth" not in ontology.node(node_id):
+            ontology.node(node_id)["depth"] = current_depth
+        else:
+            ontology.node(node_id)["depth"] = comparison_func(ontology.node(node_id)["depth"], current_depth)
+        children = set(ontology.children(node=node_id, relations=relations))
+        children.discard(node_id)
+        stack.extend([(child_id, current_depth + 1) for child_id in children])
 
 
-def set_ic_ontology_struct(ontology: Ontology, relations: List[str] = None):
+def set_ic_ontology_struct(ontology: Ontology, root_node_ids: List[str], relations: List[str] = None):
     logger.info("Setting information content values based on ontology structure")
-    roots = ontology.get_roots(relations=relations)
-    for root_id in roots:
+    start_time = time.time()
+    for root_id in root_node_ids:
         if "num_subsumers" not in ontology.node(root_id) and ("type" not in ontology.node(root_id) or
                                                               ontology.node_type(root_id) == "CLASS"):
-            _set_num_subsumers_in_subgraph(ontology=ontology, root_id=root_id, relations=relations)
-    for root_id in roots:
-        if "num_leaves" not in ontology.node(root_id) and ("type" not in ontology.node(root_id) or
-                                                           ontology.node_type(root_id) == "CLASS"):
-            _set_num_leaves_in_subgraph(ontology=ontology, root_id=root_id, relations=relations)
-    for root_id in roots:
+            set_num_subsumers(ontology=ontology, root_id=root_id, relations=relations)
+    for root_id in root_node_ids:
+        set_leaf_sets(ontology=ontology, root_id=root_id, relations=relations)
+        set_num_leaves(ontology=ontology, root_id=root_id, relations=relations)
+    for root_id in root_node_ids:
         if "depth" not in ontology.node(root_id) and ("type" not in ontology.node(root_id) or
                                                       ontology.node_type(root_id) == "CLASS"):
             set_all_depths_in_subgraph(ontology=ontology, root_id=root_id, relations=relations)
-    for root_id in roots:
+    for root_id in root_node_ids:
         if "type" not in ontology.node(root_id) or ontology.node_type(root_id) == "CLASS":
-            _set_information_content_in_subgraph(ontology=ontology, root_id=root_id,
-                                                 maxleaves=ontology.node(root_id)["num_leaves"], relations=relations)
-    logger.info("Finished setting information content values")
+            set_information_content_in_subgraph(ontology=ontology, root_id=root_id,
+                                                maxleaves=ontology.node(root_id)["num_leaves"], relations=relations)
+    logger.info(f"setting information content values based on ic took {time.time() - start_time} seconds")
 
 
-def set_ic_annot_freq(ontology: Ontology, annotations: AssociationSet):
+def set_ic_annot_freq(ontology: Ontology, annotations: AssociationSet, root_node_ids: List[str]):
     logger.info("Setting information content values based on annotation frequency")
     for node_id in ontology.nodes():
         node_prop = ontology.node(node_id)
@@ -138,7 +142,7 @@ def set_ic_annot_freq(ontology: Ontology, annotations: AssociationSet):
             del node_prop["tot_annot_genes"]
         if "IC" in node_prop:
             del node_prop["IC"]
-    for root_id in ontology.get_roots():
+    for root_id in root_node_ids:
         if "depth" not in ontology.node(root_id) and ("type" not in ontology.node(root_id) or
                                                       ontology.node_type(root_id) == "CLASS"):
             set_all_depths_in_subgraph(ontology=ontology, root_id=root_id)
@@ -148,8 +152,7 @@ def set_ic_annot_freq(ontology: Ontology, annotations: AssociationSet):
     for node_id in ontology.nodes():
         node_pr = ontology.node(node_id)
         node_pr["rel_annot_genes"] = node_gene_map[node_id]
-    for root_id in ontology.get_roots():
-        _set_tot_annots_in_subgraph(ontology, root_id)
+    set_tot_annots(ontology)
     for node_prop in ontology.nodes().values():
         if "tot_annot_genes" not in node_prop:
             node_prop["tot_annot_genes"] = set()
@@ -164,65 +167,144 @@ def set_ic_annot_freq(ontology: Ontology, annotations: AssociationSet):
     logger.info("Finished setting information content values")
 
 
-def _set_tot_annots_in_subgraph(ontology: Ontology, root_id: str, relations: List[str] = None):
-    if "tot_annot_genes" not in ontology.node(root_id):
-        children = set(ontology.children(root_id, relations=relations))
-        children.discard(root_id)
-        children = list(children)
-        ontology.node(root_id)["tot_annot_genes"] = ontology.node(root_id)["rel_annot_genes"] | set(
-            [annot_gene for child_id in children for annot_gene in
-             _set_tot_annots_in_subgraph(ontology, child_id)])
-    return ontology.node(root_id)["tot_annot_genes"]
+def set_tot_annots(ontology: Ontology, relations: List[str] = None):
+    """
+    Calculate the total number of annotated genes in a subgraph of the ontology
+
+    Args:
+        ontology (Ontology): the ontology
+        relations (List[str]): list of relations to consider
+
+    Returns:
+        Set[str]: the set of all annotated genes in the subgraph
+    """
+    logger.info("Setting total annotation counts")
+    start_time = time.time()
+    for node_id in ontology.nodes():
+        if "rel_annot_genes" in ontology.node(node_id) and ontology.node(node_id)["rel_annot_genes"]:
+            if "tot_annot_genes" not in ontology.node(node_id):
+                ontology.node(node_id)["tot_annot_genes"] = set()
+            ontology.node(node_id)["tot_annot_genes"].update(ontology.node(node_id)["rel_annot_genes"])
+            for ancestor_id in ontology.ancestors(node_id):
+                if "tot_annot_genes" not in ontology.node(ancestor_id):
+                    ontology.node(ancestor_id)["tot_annot_genes"] = set()
+                ontology.node(ancestor_id)["tot_annot_genes"].update(ontology.node(node_id)["rel_annot_genes"])
+    logger.info(f"setting tot annotation counts took {time.time() - start_time} seconds")
 
 
-def _set_num_subsumers_in_subgraph(ontology: Ontology, root_id: str, relations: List[str] = None):
-    if "num_subsumers" not in ontology.node(root_id):
-        parents = set(ontology.parents(root_id))
-        parents.discard(root_id)
+def set_num_subsumers(ontology: Ontology, root_id: str, relations: List[str] = None):
+    """
+    Calculate the number of subsumers for all nodes in the ontology
+
+    Args:
+        ontology (Ontology): the ontology
+        root_id (str): the ID of the root term of the subgraph to process
+        relations (List[str]): list of relations to consider
+    """
+    logger.info("Setting number of subsumers")
+    start_time = time.time()
+    visited = set()
+    stack = [(root_id, set())]
+    while stack:
+        node_id, subsumers = stack.pop()
+        subsumers = set(subsumers)
+        if node_id in visited:
+            continue
+        parents = set(ontology.parents(node_id))
+        parents.discard(node_id)
         parents = list(parents)
         if not parents or all(["set_subsumers" in ontology.node(parent) for parent in parents]):
-            subsumers = {subsumer for parent in parents for subsumer in ontology.node(parent)["set_subsumers"]} | \
-                        {root_id}
-            ontology.node(root_id)["num_subsumers"] = len(subsumers)
-            ontology.node(root_id)["set_subsumers"] = subsumers
-            for child_id in ontology.children(node=root_id):
-                _set_num_subsumers_in_subgraph(ontology, child_id, relations)
+            subsumers |= {subsumer for parent in parents for subsumer in ontology.node(parent)["set_subsumers"]} | {node_id}
+            ontology.node(node_id)["num_subsumers"] = len(subsumers)
+            ontology.node(node_id)["set_subsumers"] = subsumers
+            children = set(ontology.children(node=node_id))
+            children.discard(node_id)
+            stack.extend([(child_id, subsumers) for child_id in children])
+            visited.add(node_id)
+    logger.info(f"setting num subsumers took {time.time() - start_time} seconds")
 
 
-def _set_num_leaves_in_subgraph(ontology: Ontology, root_id: str, relations: List[str] = None):
-    if "set_leaves" in ontology.node(root_id):
-        return ontology.node(root_id)["set_leaves"]
-    children = set(ontology.children(node=root_id))
-    children.discard(root_id)
-    children = list(children)
-    if not children:
-        leaves = {root_id}
-        num_leaves = 0
-    else:
-        leaves = {leaf for child_id in children for leaf in
-                  _set_num_leaves_in_subgraph(ontology=ontology, root_id=child_id, relations=relations)}
-        num_leaves = len(leaves)
-    ontology.node(root_id)["num_leaves"] = num_leaves
-    ontology.node(root_id)["set_leaves"] = leaves
-    return leaves
+def set_leaf_sets(ontology: Ontology, root_id: str, relations: List[str] = None):
+    """
+    Set the set of leaves for each node in the ontology
 
-
-def _set_information_content_in_subgraph(ontology: Ontology, root_id: str, maxleaves: int, relations: List[str] = None):
-    node = ontology.node(root_id)
-    if str(root_id) == root_id and "ARTIFICIAL_NODE:" in root_id:
-        node["IC"] = 0
-    else:
-        if "num_leaves" in node and "num_subsumers" in node:
-            node["IC"] = -math.log((float(node["num_leaves"]) / node["num_subsumers"] + 1) / (maxleaves + 1))
+    Args:
+        ontology (Ontology): the ontology
+        root_id (str): the ID of the root term of the subgraph to process
+        relations (List[str]): list of relations to consider
+    """
+    logger.info("Setting leaf sets")
+    start_time = time.time()
+    visited = set()
+    stack = [root_id]
+    while stack:
+        node_id = stack.pop()
+        if node_id in visited:
+            continue
+        visited.add(node_id)
+        children = set(ontology.children(node=node_id, relations=relations))
+        children.discard(node_id)
+        if not children:
+            for ancestor in ontology.ancestors(node=node_id, relations=relations):
+                if "set_leaves" not in ontology.node(ancestor):
+                    ontology.node(ancestor)["set_leaves"] = set()
+                ontology.node(ancestor)["set_leaves"].add(node_id)
         else:
-            logger.warning("Disconnected node: " + root_id)
+            stack.extend([child_id for child_id in children])
+    logger.info(f"setting leaf sets took {time.time() - start_time} seconds")
+
+
+def set_num_leaves(ontology: Ontology, root_id: str, relations: List[str] = None):
+    """
+    Set the set of leaves for each node in the ontology
+
+    Args:
+        ontology (Ontology): the ontology
+        root_id (str): the ID of the root term of the subgraph to process
+        relations (List[str]): list of relations to consider
+    """
+    logger.info("Setting number of leaves")
+    start_time = time.time()
+    for node_id in ontology.nodes():
+        if "set_leaves" in ontology.node(node_id):
+            ontology.node(node_id)["num_leaves"] = len(ontology.node(node_id)["set_leaves"])
+        else:
+            ontology.node(node_id)["num_leaves"] = 0
+    logger.info(f"setting num leaves took {time.time() - start_time} seconds")
+
+
+def set_information_content_in_subgraph(ontology: Ontology, root_id: str, maxleaves: int, relations: List[str] = None):
+    """
+    Calculate the information content for a node in a subgraph of the ontology
+
+    Args:
+        ontology (Ontology): the ontology
+        root_id (str): the ID of the root term of the subgraph to process
+        maxleaves (int): the maximum number of leaves in the subgraph
+        relations (List[str]): list of relations to consider
+    """
+    logger.info("Calculating IC values")
+    start_time = time.time()
+    visited = set()
+    stack = [root_id]
+    while stack:
+        node_id = stack.pop()
+        if node_id in visited:
+            continue
+        visited.add(node_id)
+        node = ontology.node(node_id)
+        if str(node_id) == node_id and "ARTIFICIAL_NODE:" in node_id:
             node["IC"] = 0
-    children = set(ontology.children(node=root_id, relations=relations))
-    children.discard(root_id)
-    children = list(children)
-    for child_id in children:
-        _set_information_content_in_subgraph(ontology=ontology, root_id=child_id, maxleaves=maxleaves,
-                                             relations=relations)
+        else:
+            if "num_leaves" in node and "num_subsumers" in node:
+                node["IC"] = -math.log((float(node["num_leaves"]) / node["num_subsumers"] + 1) / (maxleaves + 1))
+            else:
+                logger.warning("Disconnected node: " + str(node_id))
+                node["IC"] = 0
+        children = set(ontology.children(node=node_id, relations=relations))
+        children.discard(node_id)
+        stack.extend(list(children))
+    logger.info(f"calculating ic values took {time.time() - start_time} seconds")
 
 
 def node_is_in_branch(ontology: Ontology, node_id: str, branch_root_ids: List[str]):
